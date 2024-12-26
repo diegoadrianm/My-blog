e0e985f21d69406ad30a3929b3febcd51e8f4eaf
diegoadrianm <diegomorduar@gmail.com>
2024-12-26 13:57:14 -0600

    Dummy data and 404 page

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name='blog-index'),
     path("posts", views.posts, name='blog-posts'),
-    path("posts/<slug:slug>", views.post_details, name='blog-posts-details'),
+    path("posts/<slug:slug>", views.post_details, name='blog-post-details'),
 ]
